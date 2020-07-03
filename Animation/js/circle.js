e6af0694dd05a760bb5ff63e612cc79a18a07d02
Rpratik13 <r.pratik013@gmail.com>
2020-07-03 09:34:52 +0545

    Fix time delay

--- Animation/js/circle.js
+++ Animation/js/circle.js
@@ -4,12 +4,10 @@ function Circle(idx, color, x) {
   this.sizeCounter = idx;
   this.y = -idx * 10 + 60;
   this.x = x;
-  this.angle = 0;
 
-  this.changeSize = function () {
-    this.angle = (this.angle + 1) % 360;
-    this.size = 3 + 3 * Math.cos(this.angle * TO_RADIAN);
-    this.y = -idx * 10 + 60 + (30 * Math.sin(this.angle * TO_RADIAN));
+  this.changeSize = function (angle) {
+    this.size = 3 + 3 * Math.cos(angle * TO_RADIAN);
+    this.y = -idx * 10 + 60 + (30 * Math.sin(angle * TO_RADIAN));
   }
 
   this.checkNegative = function (val) {
@@ -20,12 +18,12 @@ function Circle(idx, color, x) {
   }
 
 
-  this.draw = function (ctx, y) {
+  this.draw = function (ctx, angle) {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.checkNegative(this.size), 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
-    this.changeSize();
+    this.changeSize(angle);
   }
 }
--- Animation/js/column.js
+++ Animation/js/column.js
@@ -3,33 +3,22 @@ function Column(idx) {
   this.x = 250 - (idx % 16) * 15
   this.colors = ['#ffae73', '#feab77', '#fea57c', '#fe9d84', '#fd9b87', '#f9958c', '#f49091', '#ef8b97', '#ea869b', '#e6829e', '#e17da4'];
   this.y = 30;
-  this.angle = 0;
+  this.angle = -idx * 10;
 
-  if (idx < 16) {
-    this.draw = -Math.sin((idx / 16) * 90 * TO_RADIAN) * 20;
-  } else {
-    // this.draw = -(idx % 16) * 5 - 90;
-    this.draw = -Math.sin(((idx % 16) / 16) * 90 * TO_RADIAN) * 20 - Math.sin(90 * TO_RADIAN / 16) * 160;
+  if (idx > 16) {
+    this.angle += 360;
   }
+
 
   for (var i = 0; i < 11; i++) {
     this.circles.push(new Circle(-i, COLORS[i], this.x))
   }
 
-
-  this.moveDown = function () {
-    this.y = 30 + (60 * Math.sin(this.angle * TO_RADIAN));
-    this.angle = (this.angle + 1) % 180;
-  }
-
   this.drawColumn = function (ctx) {
-    if (this.draw >= 0) {
-      for (var i = 0; i < 11; i++) {
-        this.circles[i].draw(ctx, this.y);
-      }
-      this.moveDown();
-    } else {
-      this.draw += Math.sin(90 * TO_RADIAN / 16);
+
+    for (var i = 0; i < 11; i++) {
+      this.circles[i].draw(ctx, this.angle);
     }
+    this.angle = (this.angle + 1) % 360;
   }
 }
