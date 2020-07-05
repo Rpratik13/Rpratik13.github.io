09ad3d0a59bd68e8f576fe38e27bd3e0c796f53f
Rpratik13 <r.pratik013@gmail.com>
2020-07-05 23:04:32 +0545

    Add inventory and crafting

--- Terraria/js/player.js
+++ Terraria/js/player.js
@@ -11,6 +11,20 @@ function Player() {
   this.jumpCounter = 0;
   this.swinging = false;
   this.swingCounter = 0;
+  this.showInventory = false;
+  this.items = { 'sword': 1 };
+  this.itemsName = ['sword'];
+  this.tilesCollected = [];
+  this.inventory = new Inventory(this);
+  this.weapon;
+  this.helm;
+  this.chest;
+  this.boot;
+  this.armor = 0;
+  this.pickPower = 1;
+  this.axePower = 1;
+  this.damage = 5;
+
   this.draw = function (ctx, tiles) {
     ctx.save()
     if (this.left) {
@@ -31,9 +45,9 @@ function Player() {
       this.pose = (this.pose + 1) % 11;
     }
     if (this.x > 0 &&
-      (tiles[Math.floor(this.y) + 3][Math.ceil(this.x)] > 4 || tiles[Math.floor(this.y) + 3][Math.ceil(this.x)] < 1) &&
-      (tiles[Math.floor(this.y) + 2][Math.ceil(this.x)] > 4 || tiles[Math.floor(this.y) + 2][Math.ceil(this.x)] < 1) &&
-      (tiles[Math.floor(this.y) + 1][Math.ceil(this.x)] > 4 || tiles[Math.floor(this.y) + 1][Math.ceil(this.x)] < 1)) {
+      ((tiles[Math.floor(this.y) + 3][Math.ceil(this.x)] > 4 || tiles[Math.floor(this.y) + 3][Math.ceil(this.x)] < 1) && tiles[Math.floor(this.y) + 3][Math.ceil(this.x)] != 36) &&
+      ((tiles[Math.floor(this.y) + 2][Math.ceil(this.x)] > 4 || tiles[Math.floor(this.y) + 2][Math.ceil(this.x)] < 1) && tiles[Math.floor(this.y) + 2][Math.ceil(this.x)] != 36) &&
+      ((tiles[Math.floor(this.y) + 1][Math.ceil(this.x)] > 4 || tiles[Math.floor(this.y) + 1][Math.ceil(this.x)] < 1) && tiles[Math.floor(this.y) + 1][Math.ceil(this.x)] != 36)) {
       this.x -= 1 / 4;
     }
   }
@@ -43,9 +57,9 @@ function Player() {
       this.pose = (this.pose + 1) % 11;
     }
     if (this.x < 48 &&
-      (tiles[Math.floor(this.y) + 3][Math.floor(this.x) + 3] > 4 || tiles[Math.floor(this.y) + 3][Math.floor(this.x) + 3] < 1) &&
-      (tiles[Math.floor(this.y) + 2][Math.floor(this.x) + 3] > 4 || tiles[Math.floor(this.y) + 2][Math.floor(this.x) + 3] < 1) &&
-      (tiles[Math.floor(this.y) + 1][Math.floor(this.x) + 3] > 4 || tiles[Math.floor(this.y) + 1][Math.floor(this.x) + 3] < 1)) {
+      ((tiles[Math.floor(this.y) + 3][Math.floor(this.x) + 3] > 4 || tiles[Math.floor(this.y) + 3][Math.floor(this.x) + 3] < 1) && tiles[Math.floor(this.y) + 3][Math.floor(this.x) + 3] != 36) &&
+      ((tiles[Math.floor(this.y) + 2][Math.floor(this.x) + 3] > 4 || tiles[Math.floor(this.y) + 2][Math.floor(this.x) + 3] < 1) && tiles[Math.floor(this.y) + 2][Math.floor(this.x) + 3] != 36) &&
+      ((tiles[Math.floor(this.y) + 1][Math.floor(this.x) + 3] > 4 || tiles[Math.floor(this.y) + 1][Math.floor(this.x) + 3] < 1) && tiles[Math.floor(this.y) + 1][Math.floor(this.x) + 3] != 36)) {
       this.x += 1 / 4;
     }
   }
@@ -53,6 +67,8 @@ function Player() {
   this.fall = function (tiles) {
     this.pose = 11;
     if (this.y < 36 &&
+      (tiles[Math.floor(this.y) + 4][Math.floor(this.x) + 1] != 36) &&
+      (tiles[Math.floor(this.y) + 4][Math.floor(this.x) + 2] != 36) &&
       (tiles[Math.floor(this.y) + 4][Math.floor(this.x) + 1] == 0 || tiles[Math.floor(this.y) + 4][Math.floor(this.x) + 1] > 4) &&
       (tiles[Math.floor(this.y) + 4][Math.floor(this.x) + 2] == 0 || tiles[Math.floor(this.y) + 4][Math.floor(this.x) + 2] > 4)) {
       this.y += 1 / 4;
@@ -68,7 +84,11 @@ function Player() {
       this.jumping = true;
       this.y -= 1 / 4;
       this.jumpCounter += 1;
-      if (this.jumpCounter == 16 || (tiles[Math.ceil(this.y)][Math.round(this.x) + 2] != 0 || tiles[Math.ceil(this.y)][Math.round(this.x) + 1] != 0)) {
+      if (this.jumpCounter == 16 ||
+        (tiles[Math.ceil(this.y)][Math.round(this.x) + 2] == 36) ||
+        (tiles[Math.ceil(this.y)][Math.round(this.x) + 1] == 36) ||
+        (tiles[Math.ceil(this.y)][Math.round(this.x) + 2] > 0 && tiles[Math.ceil(this.y)][Math.round(this.x) + 2] < 5) ||
+        (tiles[Math.ceil(this.y)][Math.round(this.x) + 1] > 0 && tiles[Math.ceil(this.y)][Math.round(this.x) + 1] < 5)) {
         this.jumpCounter = 0;
         this.jumping = false;
         this.falling = true;
@@ -90,6 +110,116 @@ function Player() {
     }
     this.swingCounter += 1;
 
+  }
+
+  this.itemPickup = function (world) {
+    var tiles = world.droppedTiles;
+    for (var i = 0; i < tiles.length; i++) {
+      if (Math.floor(tiles[i].tileX) > this.x + 1 || Math.ceil(tiles[i].tileX) < this.x ||
+        tiles[i].tileY > this.y + 1 || tiles[i].tileY < this.y + 1) {
+        if (tiles[i].tileX > this.x + 1) {
+          tiles[i].moveLeft();
+        } else if (tiles[i].tileX < this.x) {
+          tiles[i].moveRight();
+        }
+
+        if (tiles[i].tileY > this.y + 1) {
+          tiles[i].moveUp();
+        } else if (tiles[i].tileY < this.y + 1) {
+          tiles[i].moveDown();
+        }
+      } else {
+        this.tilesCollected.push(i);
+        if (tiles[i].type in this.items) {
+          this.items[tiles[i].type] += 1;
+        } else {
+          this.items[tiles[i].type] = 1;
+          this.itemsName.push(tiles[i].type);
+        }
+      }
+    }
+
+    for (var i = 0; i < this.tilesCollected.length; i++) {
+      world.droppedTiles.splice(this.tilesCollected[i], 1);
+      this.tilesCollected.shift();
+    }
+  }
+
+
+  this.checkArmor = function () {
+    console.log('hell')
+    var chest;
+    var helm
+    var boot;
+
+    if (this.helm == undefined) {
+      helm = 0;
+    } else if (this.helm == 'silver_helm') {
+      helm = 3;
+    } else if (this.helm == 'gold_helm') {
+      helm = 5;
+    }
+
+    if (this.chest == undefined) {
+      chest = 0;
+    } else if (this.chest == 'silver_chest') {
+      chest = 3;
+    } else if (this.chest == 'gold_chest') {
+      chest = 5;
+    }
+
+    if (this.boot == undefined) {
+      boot = 0;
+    } else if (this.boot == 'silver_boot') {
+      boot = 3;
+    } else if (this.boot == 'gold_boot') {
+      boot = 5;
+    }
+
+
+    this.armor = helm + chest + boot;
+
+  }
+
+  this.checkPower = function () {
+    if (this.weapon == undefined) {
+      this.damage = 5;
+      this.pickPower = 1;
+      this.axePower = 1;
+    } else if (this.weapon == 'sword') {
+      this.damage = 7;
+      this.pickPower = 0;
+      this.axePower = 0;
+    } else if (this.weapon == 'silver_sword') {
+      this.damage = 9;
+      this.pickPower = 0;
+      this.axePower = 0;
+    } else if (this.weapon == 'gold_sword') {
+      this.damage = 15;
+      this.pickPower = 0;
+      this.axePower = 0;
+    } else if (this.weapon == 'silver_pick') {
+      this.damage = 5;
+      this.pickPower = 2;
+      this.axePower = 0;
+    } else if (this.weapon == 'gold_pick') {
+      this.damage = 6;
+      this.pickPower = 4;
+      this.axePower = 0;
+    } else if (this.weapon == 'silver_axe') {
+      this.damage = 5;
+      this.pickPower = 0;
+      this.axePower = 2;
+    } else if (this.weapon == 'gold_axe') {
+      this.damage = 6;
+      this.pickPower = 0;
+      this.axePower = 4;
+    } else {
+      this.damage = 5;
+      this.pickPower = 0;
+      this.axePower = 0;
+    }
+    console.log(this.axePower);
   }
 
 }
