744d477893a76cedc3470b2df834cb8f031903ec
Rpratik13 <r.pratik013@gmail.com>
2020-07-09 09:02:01 +0545

    Generate Enemy

--- Terraria/js/game.js
+++ Terraria/js/game.js
@@ -1,4 +1,5 @@
 function Game() {
+  let that = this;
   let game = document.getElementById('terraria');
   let canvas = document.createElement('canvas');
   canvas.setAttribute('class', 'canvas');
@@ -10,10 +11,30 @@ function Game() {
   let player = new Player();
   ctx.font = "10px Arial";
   ctx.fillStyle = 'white';
-  let enemies = [new Eye(ctx, 18, 14), new Eye(ctx, 28, 14)];
+  let enemies = [];
   let timer = 0;
+  let enemiesDead = [];
 
-  game.onmousedown = function (e) {
+  let background = [new Image, new Image];
+  background[0].src = 'images/bg0.png';
+  background[1].src = 'images/bg1.png';
+  let bg_under = new Image;
+  bg_under.src = 'images/bg2.png';
+  let bg_num = 0;
+  let sound = document.createElement("audio");
+  sound.src = 'audio/bg.mp3';
+  sound.setAttribute("preload", "auto");
+  sound.setAttribute("controls", "none");
+  sound.style.display = "none";
+  document.body.appendChild(sound);
+  this.die = sound;
+  this.die.load();
+
+
+  document.onmousedown = function (e) {
+    console.log(this)
+    that.die.play();
+
     var cursorX = e.pageX;
     var cursorY = e.pageY;
     if (player.showInventory) {
@@ -52,7 +73,8 @@ function Game() {
 
   function run() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
+    ctx.drawImage(background[bg_num], 0, 0, CANVAS_WIDTH, 300)
+    ctx.drawImage(bg_under, 0, 300, CANVAS_WIDTH, 620)
     world.drawWorld(ctx);
     world.drawDroppedTiles(ctx);
     player.draw(ctx, world.world);
@@ -61,7 +83,7 @@ function Game() {
     } else if (player.jumping) {
       player.jump(world.world);
     } else if (player.swinging) {
-      player.swing(ctx);
+      player.swing(ctx, enemies);
     }
     player.displayHealth(ctx);
     if (player.goLeft) {
@@ -74,14 +96,29 @@ function Game() {
     }
     player.itemPickup(world);
     // slime.draw(player, world);
-    timer += 1;
-    // if (timer == 100) {
-    //   enemies.push(new Zombie(ctx, Math.floor(Math.random() * (45 - 2) + 2), 0))
-    // }
+    timer = (timer + 1) % 20000;
+    if (timer % 500 == 0) {
+      if (timer < 10000) {
+        enemies.push(new Slime(ctx, Math.floor(Math.random() * (45 - 2) + 2), 0))
+        bg_num = 0;
+      } else {
+        enemies.push(new Eye(ctx, Math.floor(Math.random() * (45 - 2) + 2), 0))
+        enemies.push(new Zombie(ctx, Math.floor(Math.random() * (45 - 2) + 2), 0))
+        bg_num = 1;
+      }
+    }
 
     for (var i = 0; i < enemies.length; i++) {
       enemies[i].draw(player, world);
+      if (!enemies[i].alive) {
+        enemiesDead.push(i)
+      }
     }
+
+    for (var i = 0; i < enemiesDead.length; i++) {
+      enemies.splice(enemiesDead[i], 1);
+    }
+    enemiesDead = [];
     requestAnimationFrame(run);
   }
   run()
