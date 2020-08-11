09812c0f8a5d3029cb08de9ba7681ab22bc7dbff
Pratik Rajbhandari <31899424+Rpratik13@users.noreply.github.com>
2020-08-11 20:15:23 +0545

    Add comments

--- Express_Server_Code/server.js
+++ Express_Server_Code/server.js
@@ -9,19 +9,20 @@ const app     = express();
 app.get('/', function(req, res) {
   res.end(`Instructions \n
            1. Write \n
-           Go to localhost:${config.port}/write/fileName/fileNontent/ \n\n
+           Go to localhost:${config.port}/write/<fileName>/<fileContent>/ \n\n
            2. Read \n
-           Go to localhost:${config.port}/read/fileName/ \n\n
+           Go to localhost:${config.port}/read/<fileName>/ \n\n
            3. Rename \n
-           Go to localhost:${config.port}/rename/oldFileName/newFileName/ \n\n
+           Go to localhost:${config.port}/rename/<oldFileName>/<newFileName>/ \n\n
            4. Delete \n
-           Go to localhost:${config.port}/delete/fileName/ \n\n`
+           Go to localhost:${config.port}/delete/<fileName>/ \n\n`
           );
 });
 
 app.get('/write/:fileName/:fileContent', function(req, res) {
   const fileContent  = req.params.fileContent;
-  const fileName = req.params.fileName;
+  const fileName     = req.params.fileName;
+ 
   fileOperation.writeFile(fileName, fileContent)
   .then(function() {
     res.end(`File ${fileName} has been created with content '${fileContent}'`);
@@ -33,6 +34,7 @@ app.get('/write/:fileName/:fileContent', function(req, res) {
 
 app.get('/read/:fileName', function(req, res) {
   const fileName = req.params.fileName;
+ 
   fileOperation.readFile(fileName)
   .then(function(data) {
     res.end(data);
@@ -43,8 +45,9 @@ app.get('/read/:fileName', function(req, res) {
 });
 
 app.get('/rename/:oldFileName/:newFileName', function(req, res) {
-  const oldFileName = req.params.oldFileName;
   const newFileName = req.params.newFileName;
+  const oldFileName = req.params.oldFileName;
+ 
   fileOperation.renameFile(oldFileName, newFileName)
   .then(function() {
     res.end(`Successfully renamed file ${oldFileName} to ${newFileName}`);
@@ -56,6 +59,7 @@ app.get('/rename/:oldFileName/:newFileName', function(req, res) {
 
 app.get('/delete/:fileName', function(req, res) {
   const fileName = req.params.fileName;
+ 
   fileOperation.deleteFile(fileName)
   .then(function() {
     res.end(`Successfully deleted file ${fileName}`);
--- Express_Server_Code/src/fileOperations.js
+++ Express_Server_Code/src/fileOperations.js
@@ -4,6 +4,11 @@ const config = require('../configs');
 // Import Modules
 const fs = require('fs');
 
+/**
+ * 
+ * @param {string} fileName    Name of the file to write on.
+ * @param {string} fileContent Content to store in the file. 
+ */
 function writeFile(fileName, fileContent) {
   return new Promise(function(resolve, reject) {
     fs.writeFile(`${config.filePath}${fileName}`, fileContent, function(err, done) {
@@ -14,9 +19,13 @@ function writeFile(fileName, fileContent) {
         resolve(done);
       }
     });  
-  })
+  });
 }
 
+/**
+ * 
+ * @param {string} fileName Name of the file to read. 
+ */
 function readFile(fileName) {
   return new Promise(function(resolve, reject) {
     fs.readFile(`${config.filePath}${fileName}`, 'UTF-8', function(err, done) {
@@ -27,9 +36,14 @@ function readFile(fileName) {
         resolve(done);
       }
     });  
-  })
+  });
 }
 
+/**
+ * 
+ * @param {string} fileOldName Name of the file to be renamed. 
+ * @param {string} fileNewName New name to be given to the renamed file.
+ */
 function renameFile(fileOldName, fileNewName){
   return new Promise(function(resolve, reject) {
     fs.rename(`${config.filePath}${fileOldName}`, `${config.filePath}${fileNewName}`, function(err, done) {
@@ -40,9 +54,13 @@ function renameFile(fileOldName, fileNewName){
         resolve(done);
       }
     });   
-  }) 
+  }); 
 }
 
+/**
+ * 
+ * @param {string} fileName Name of the file to be deleted. 
+ */
 function deleteFile(fileName) {
   return new Promise(function(resolve, reject) {
     fs.unlink(`${config.filePath}${fileName}`, function(err, done) {
